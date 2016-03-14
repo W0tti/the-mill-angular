faf3aa655202d1cde67e3f29a752e3d047cea670
W0tti <jackwatkinson@ds2.local>
2016-03-14 12:33:41 +0000

    Thirtieth commit
    Added the processing code form the form.

--- js/millApp.js
+++ js/millApp.js
@@ -67,4 +67,24 @@ millApp.controller("formController", function($scope, $http){
     // create a blank object to hold our form information
     $scope.formData = {};
     
+    $scope.processForm = function() {
+        $http({
+        method  : 'POST',
+        url     : 'process.php',
+        data    : $.param($scope.formData),  // pass in data as strings
+        headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  // set the headers so angular passing info as form data (not request payload)
+        })
+        .success(function(data) {
+            console.log(data);
+
+            if (!data.success) {
+              // if not successful, bind errors to error variables
+              $scope.errorName = data.errors.name;
+              $scope.errorEmail = data.errors.email;
+            } else {
+              // if successful, bind success message to message
+              $scope.message = data.message;
+            }
+        });
+    };
 });
